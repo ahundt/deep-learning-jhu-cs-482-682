d2f178e4f05065df5f8752d891555567ea5a7b9c
Andrew Hundt <athundt@gmail.com>
2018-02-07 15:52:01 -0500

    fix crash

--- 1_fashion_mnist.py
+++ 1_fashion_mnist.py
@@ -119,13 +119,14 @@ class Net(nn.Module):
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = self.bn1(x)
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
         x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+
+model = Net()
 
 writer.add_graph(model, images[:2])
 
