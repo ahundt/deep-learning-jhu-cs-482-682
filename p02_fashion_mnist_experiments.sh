85e551a25c3ac6fe9582f3deac7c04d4469aa4a5
Andrew Hundt <ATHundt@gmail.com>
2018-02-12 21:34:38 -0500

    sh script param fix

--- p02_fashion_mnist_experiments.sh
+++ p02_fashion_mnist_experiments.sh
@@ -1,15 +1,15 @@
 # Question 1 Default settings
 python p02_fashion_mnist.py --dataset mnist --epochs 10 --name q1_default
-python p02_fashion_mnist.py --dataset fashion_mnist --epochs 10 --name q1_default  --logdir ../data/q1
+python p02_fashion_mnist.py --dataset fashion_mnist --epochs 10 --name q1_default  --data_dir ../data/q1
 
 # Question 2
-python p02_fashion_mnist.py --dataset mnist --epochs 20 --name q1_20_epochs --logdir ../data/q1
-python p02_fashion_mnist.py --dataset fashion_mnist --epochs 20 --name q1_20_epochs --logdir ../data/q1
+python p02_fashion_mnist.py --dataset mnist --epochs 20 --name q1_20_epochs --data_dir ../data/q1
+python p02_fashion_mnist.py --dataset fashion_mnist --epochs 20 --name q1_20_epochs --data_dir ../data/q1
 
 
 # Question 3
-python p02_fashion_mnist.py --dataset fashion_mnist --lr 0.1 --name q2_20_epochs --logdir ../data/q2
-python p02_fashion_mnist.py --dataset fashion_mnist --lr 0.001 --name q2_20_epochs --logdir ../data/q2
+python p02_fashion_mnist.py --dataset fashion_mnist --lr 0.1 --name q2_20_epochs --data_dir ../data/q2
+python p02_fashion_mnist.py --dataset fashion_mnist --lr 0.001 --name q2_20_epochs --data_dir ../data/q2
 
 # ...and so on, hopefully you have the idea now.
 
